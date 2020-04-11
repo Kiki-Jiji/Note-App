10c33ce1f55ca32634499604153171b61d2bbc09
Joshua Halls <joshuah40@googlemail.com>
2020-04-11 23:13:56 +0100

    Updates

---
+++ precache-manifest.c37919ae3050a3d7cea498aff5b3bb30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d56564b4957ab53e05b40a09844059",
+    "url": "/Note-App/index.html"
+  },
+  {
+    "revision": "5c999b58e8afba17e595",
+    "url": "/Note-App/static/css/main.afe11932.chunk.css"
+  },
+  {
+    "revision": "d6272131c0d17caaa4e4",
+    "url": "/Note-App/static/js/2.20bc86cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Note-App/static/js/2.20bc86cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c999b58e8afba17e595",
+    "url": "/Note-App/static/js/main.f0c89d79.chunk.js"
+  },
+  {
+    "revision": "99ba5cbc18230dfa64cb",
+    "url": "/Note-App/static/js/runtime-main.ab20af7f.js"
+  }
+]);
